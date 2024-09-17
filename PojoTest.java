7d415a938df4a288d431e6d3cfb02fb500ed88c8
Rajinfo <rajesh.balasekaran@gmail.com>
2024-09-17 11:44:09 +0530

    Update PojoTest.java

--- PojoTest.java
+++ PojoTest.java
@@ -1,11 +1,15 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PojoTest {
 
     @Test
     public void testAllPojosInPackage() {
-        String packageName = "com.example.myapp.pojos"; // Your package name
-        
-        PojoTestUtility.testAllPojosInPackage(packageName);
+        String packageName = "com.example.myapp"; // Your package name
+        Set<Class<?>> ignoredClasses = new HashSet<>();
+        ignoredClasses.add(CustomException.class);  // Example of ignoring a specific exception
+        PojoTestUtility.testAllPojosInPackage(packageName, ignoredClasses);
     }
 }
