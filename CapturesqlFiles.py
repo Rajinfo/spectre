bc1f2be322ee3f6ab5899275c568e5168fd4c9e5
Rajinfo <rajesh.balasekaran@gmail.com>
2024-10-24 15:31:24 +0530

    Update CapturesqlFiles.py

--- CapturesqlFiles.py
+++ CapturesqlFiles.py
@@ -1,23 +1,24 @@
 import os
 import csv
 
-def find_db_folder_and_capture_files(root_folder, output_csv):
+def find_db_and_crudb_folders_and_capture_files(root_folder, output_csv):
     # List to hold the file information
     file_info_list = []
 
     # Walk through the directory structure
     for dirpath, dirnames, filenames in os.walk(root_folder):
-        # Check if the current directory is the 'db' folder
-        if 'db' in dirnames:
-            db_folder_path = os.path.join(dirpath, 'db')
-            # Walk through the 'db' folder and its subfolders
-            for db_dirpath, db_dirnames, db_filenames in os.walk(db_folder_path):
-                for filename in db_filenames:
-                    # Capture file information
-                    file_location = os.path.join(db_dirpath, filename)
-                    file_name, file_extension = os.path.splitext(filename)
-                    previous_folder_name = os.path.basename(os.path.dirname(file_location))
-                    file_info_list.append([file_location, file_name, file_extension, previous_folder_name])
+        # Check if the current directory contains 'db' or 'crudb' folder
+        for folder in ['db', 'crudb']:
+            if folder in dirnames:
+                target_folder_path = os.path.join(dirpath, folder)
+                # Walk through the target folder and its subfolders
+                for target_dirpath, target_dirnames, target_filenames in os.walk(target_folder_path):
+                    for filename in target_filenames:
+                        # Capture file information
+                        file_location = os.path.join(target_dirpath, filename)
+                        file_name, file_extension = os.path.splitext(filename)
+                        previous_folder_name = os.path.basename(os.path.dirname(file_location))
+                        file_info_list.append([file_location, file_name, file_extension, previous_folder_name])
 
     # Write the file information to a CSV file
     with open(output_csv, mode='w', newline='') as csv_file:
@@ -28,4 +29,4 @@ def find_db_folder_and_capture_files(root_folder, output_csv):
 # Example usage
 root_folder = 'c:/test/repo'
 output_csv = 'output.csv'
-find_db_folder_and_capture_files(root_folder, output_csv)
+find_db_and_crudb_folders_and_capture_files(root_folder, output_csv)
